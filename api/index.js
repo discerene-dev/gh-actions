169c913634b6c61be9db7ec6485314e2fde7c511
jasonfaustino <jasonfaustino24@gmail.com>
2025-07-21 21:20:34 +0800

    moved index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World!, this is a change");
 });
 
 app.listen(PORT, () => {
